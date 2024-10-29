a763c51a5cd39858c3dc82c300d1c7a50eeeadbf
JaGenn <154364849+JaGenn@users.noreply.github.com>
2024-10-29 22:23:39 +0300

    класс хищник

---
+++ src/com/dzhanbulatov/entity/Predator.java
@@ -0,0 +1,16 @@
+package com.dzhanbulatov.entity;
+
+import com.dzhanbulatov.Coordinates;
+
+public class Predator extends Creature {
+
+    public Predator(int speed, int HP, Coordinates coordinates) {
+        super(speed, HP, coordinates);
+    }
+
+
+    @Override
+    public void makeMove() {
+
+    }
+}
