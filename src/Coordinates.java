026199b185ce79b9c7036f8d82dc69bbbb038012
JaGenn <154364849+JaGenn@users.noreply.github.com>
2024-10-29 16:58:54 +0300

    Переопределил методы equals и hashCode, чтобы можно было использовать этот класс в качестве ключа в HashMap класса Map

--- src/Coordinates.java
+++ src/Coordinates.java
@@ -1,9 +1,27 @@
+import java.util.Objects;
+
 public class Coordinates {
-    public int X;
-    public int Y;
+    public Integer X;
+    public Integer Y;
 
     public Coordinates(int x, int y) {
         X = x;
         Y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Coordinates that = (Coordinates) o;
+        return X.equals(that.X) && Y.equals(that.Y);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = X.hashCode();
+        result = 31 * result + Y.hashCode();
+        return result;
     }
 }
