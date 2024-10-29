7e4b9f09dc2a953b8af785dd1371dd40c7079d77
JaGenn <154364849+JaGenn@users.noreply.github.com>
2024-10-29 23:25:08 +0300

    класс рендера, для вывода состояния карты на консоль

--- src/com/dzhanbulatov/MapConsoleRenderer.java
+++ src/com/dzhanbulatov/MapConsoleRenderer.java
@@ -1,4 +1,14 @@
 package com.dzhanbulatov;
 
 public class MapConsoleRenderer {
+
+    public static void render(Map map) {
+        for (int i = Main.Y; i >= 1; i--) {
+            for (int j = 0; j < Main.X; j++) {
+                System.out.println("X" + j + " " + "Y" + i);
+            }
+        }
+
+    }
+
 }
