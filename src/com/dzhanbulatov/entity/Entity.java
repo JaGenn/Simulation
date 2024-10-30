52436b1435b3dc626514c10a3676a574ef6be0cb
JaGenn <154364849+JaGenn@users.noreply.github.com>
2024-10-31 00:09:01 +0300

    создал поле координатов и создал конструктор, чтобы у любого объекта были свои координаты

--- src/com/dzhanbulatov/entity/Entity.java
+++ src/com/dzhanbulatov/entity/Entity.java
@@ -1,5 +1,14 @@
 package com.dzhanbulatov.entity;
 
+import com.dzhanbulatov.Coordinates;
+
 public abstract class Entity {
 
+    public Coordinates coordinates;
+
+    public Entity(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public abstract String getSprite();
 }
