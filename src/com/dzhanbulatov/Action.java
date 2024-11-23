92843b6f3e6b8c7d7c650b447e26e2c50fd6b634
JaGenn <154364849+JaGenn@users.noreply.github.com>
2024-11-23 16:18:31 +0300

    added feature to reset entities on map

--- src/com/dzhanbulatov/Action.java
+++ src/com/dzhanbulatov/Action.java
@@ -10,26 +10,30 @@ public class Action {
         this.map = map;
     }
     Scanner in = new Scanner(System.in);
+    MapConsoleRenderer renderer;
 
 
 
 
     public boolean initActions() {
         Simulation simulation = new Simulation(map);
-        System.out.println("1 - make one move\n2 - start endless simulation\n3 - exit");
+        System.out.println("1 - make one move\n2 - start endless simulation\n3 - reset entities on map\n4 - exit");
         int a = in.nextInt();
         if (a == 1) {
             simulation.nextTurn();
         } else if (a == 2) {
             simulation.stopSimulation(); // запускается параллельный поток, который будет ждать команду чтобы остановить бесконечную симуляцию
-            System.out.println("Sumulation starts in 4 seconds\nIf you want to stop simulation put \"s\" or \'\'");
+            System.out.println("Sumulation starts in 4 seconds\nIf you want to stop simulation put \"s\" or \'4\'");
             try {
                 Thread.sleep(4000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
             simulation.startSimulation();
-        } else {
+        }else if (a == 3) {
+            map.setRandomEntitiesPosition();
+            renderer.render(map);
+        }else {
             return false;
         }
         return true;
--- src/com/dzhanbulatov/Main.java
+++ src/com/dzhanbulatov/Main.java
@@ -1,19 +1,19 @@
 package com.dzhanbulatov;
 
-import java.util.Scanner;
-
 public class Main {
 
 
     public static void main(String[] args) {
         Map map = new Map(10, 10);
         Action action = new Action(map);
-        map.defaultEntitiesPositions();
-
-        do {
+        map.setRandomEntitiesPosition();
+        //action.initActions();
+        while (action.initActions()) {
+            if (!action.initActions()) {
+                break;
+            }
             action.initActions();
         }
-        while (action.initActions());
 
     }
 }
